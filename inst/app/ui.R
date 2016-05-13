04d79e00eae6861ed8f386aeb3c37064d6118ce6
Edwin de Jonge <edwindjonge@gmail.com>
2016-05-13 13:48:10 +0200

    updated shiny code

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,25 +1,35 @@
 library(shiny)
-library(cbsodata)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
   titlePanel("CBS OData"),
   
-  # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
       selectizeInput("table"
                     , "Table:"
                     , c("Loading...")
+                    , selected = NULL
                     , options = list()
-                    )
+                    ),
+      shiny::wellPanel(
+        div(
+          "table id: ", shiny::textOutput("id")
+        )
+      )
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      dataTableOutput("table_list")
+      tabsetPanel(
+        tabPanel( "query"
+                , uiOutput("buildquery")
+                #, textOutput("rquery")
+                , verbatimTextOutput("rquery")
+                ),
+        tabPanel( "table list",       
+                         dataTableOutput("table_list")
+                )
+      )      
     )
   )
 ))
